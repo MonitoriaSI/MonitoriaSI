567480e095e479ee2c0ef766fe3ff005a8db10a9
José Ricardo <zevatron@zevatron.com.br>
2018-05-18 17:28:25 -0300

    Inclusão Bootstrap e menu navbar

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,10 +2,8 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   # def after_sign_in_path_for(resource)
-  #   if true
+  #   if current_user.person.nil? == true
   #     new_person_path
-  #   else
-  #     current_user_path
   #   end
   # end
 end
