d2d25e2db13be09139b727908545c700731db8dc
José Ricardo <zevatron@zevatron.com.br>
2018-05-18 15:10:01 -0300

    //WIP: #5 Devise User references person

--- app/controllers/disciplines_controller.rb
+++ app/controllers/disciplines_controller.rb
@@ -1,5 +1,6 @@
 class DisciplinesController < ApplicationController
   before_action :set_discipline, only: [:show, :edit, :update, :destroy]
+  before_action :authenticate_user!
 
   # GET /disciplines
   # GET /disciplines.json
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180518163659) do
+ActiveRecord::Schema.define(version: 20180518173104) do
 
   create_table "disciplines", force: :cascade do |t|
     t.string "descricao"
@@ -39,6 +39,8 @@ ActiveRecord::Schema.define(version: 20180518163659) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "perfil"
+    t.integer "user_id"
+    t.index ["user_id"], name: "index_people_on_user_id"
   end
 
   create_table "semesters", force: :cascade do |t|
@@ -58,6 +60,23 @@ ActiveRecord::Schema.define(version: 20180518163659) do
     t.datetime "updated_at", null: false
     t.index ["discipline_id"], name: "index_teams_on_discipline_id"
     t.index ["semester_id"], name: "index_teams_on_semester_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer "sign_in_count", default: 0, null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string "current_sign_in_ip"
+    t.string "last_sign_in_ip"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
 end
