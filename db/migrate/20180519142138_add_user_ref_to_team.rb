8ef66c4ab01c09915604cba3a8ccf09090a0bb28
José Ricardo <zevatron@zevatron.com.br>
2018-05-19 15:22:25 -0300

    #15 Correção da referência de monitor na tabela people na Migration.

--- db/migrate/20180519142138_add_user_ref_to_team.rb
+++ db/migrate/20180519142138_add_user_ref_to_team.rb
@@ -1,5 +1,5 @@
 class AddUserRefToTeam < ActiveRecord::Migration[5.1]
   def change
-    add_reference :teams, :monitor, foreign_key: true
+    add_reference :teams, :monitor, foreign_key: { to_table: :people }
   end
 end
