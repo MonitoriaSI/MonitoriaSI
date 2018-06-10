630c7575993a292eb19dbebc4c791e7aa675529a
Jose Raimundo <j.zmais@gmail.com>
2018-06-10 18:24:23 +0000

    Tela de relatorio funcionando

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -1,4 +1,21 @@
 class ReportsController < ApplicationController
+    before_action :authenticate_user!
+
+
     def index
+        @users = User.all
+        @teams = Team.all
+        @alunos = Person.all
+        @semesters = Semester.all
+        @discliplines = Discipline.all     
     end
+
+    def create
+        print "teste"
+    end
+
+    def teste
+        print "teste"
+    end
+    
 end
