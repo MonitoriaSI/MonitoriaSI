140c49d555115eda606ffc3a59a7a6595c0e454a
vagrant <vagrant@vagrant.vm>
2018-06-12 12:44:52 +0000

    Gerar relatorio de monitorias

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -1,21 +1,12 @@
 class ReportsController < ApplicationController
     before_action :authenticate_user!
-
-
     def index
-        @users = User.all
-        @teams = Team.all
-        @alunos = Person.all
-        @semesters = Semester.all
-        @discliplines = Discipline.all     
-    end
-
-    def create
-        print "teste"
-    end
-
-    def teste
-        print "teste"
-    end
-    
+        @teams = Team.all  
+        @events = Event.all
+        if params[:team_id]
+            @events = Event.where(team_id: params[:team_id])
+        else
+            @events = Event.all
+        end
+    end  
 end
