d7aec6572dcbbb60a0e2d8315cb425ab4ba529b9
José Ricardo <zevatron@zevatron.com.br>
2018-06-15 22:26:37 -0300

    Correção do redirecionamento ao editar/excluir um evento

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -52,7 +52,7 @@ class EventsController < ApplicationController
   def update
     respond_to do |format|
       if @event.update(event_params)
-        format.html { redirect_to @event, notice: 'Event was successfully updated.' }
+        format.html { redirect_to [@team,@event], notice: 'Event was successfully updated.' }
         format.json { render :show, status: :ok, location: @event }
       else
         format.html { render :edit }
@@ -66,7 +66,7 @@ class EventsController < ApplicationController
   def destroy
     @event.destroy
     respond_to do |format|
-      format.html { redirect_to events_url, notice: 'Event was successfully destroyed.' }
+      format.html { redirect_to team_url(@team), notice: 'Event was successfully destroyed.' }
       format.json { head :no_content }
     end
   end
