9c8d9a3e9c534b985b34ffa6984d0feb86f7be6c
Unknown <alfit@oi.com.br>
2018-07-05 14:35:42 -0300

    #78-Não duplicação de disciplina e semestre

--- app/models/team.rb
+++ app/models/team.rb
@@ -3,4 +3,5 @@ class Team < ApplicationRecord
   belongs_to :semester
   belongs_to :monitor , class_name:"Person", foreign_key: "monitor_id", optional: true
   has_many :events
+  #validates :discipline, uniqueness: true
 end
