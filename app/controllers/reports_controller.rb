084e763990238a89db2cb2801ac40cdf97165d1d
Unknown <alexandrepb@gmail.com>
2018-07-11 11:08:57 -0300

    #83 Gerar relatório de monitorias mais bem avaliadas

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -3,6 +3,7 @@ class ReportsController < ApplicationController
     def index
         @teams = Team.all  
         @events = Event.all
+        @evaluations = Evaluation.all
         if params[:team_id]
             @events = Event.where(team_id: params[:team_id])
         else
